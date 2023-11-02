d3bef053a8ea5ac4c880080a23815224bca8c18e
oomatinai <damirbekuuluoomat@gmail.com>
2023-11-02 19:56:53 +0600

    улучшенная версия01

--- src/main/java/com/example/demo/MoneyTrackerApp.java
+++ src/main/java/com/example/demo/MoneyTrackerApp.java
@@ -1,17 +1,30 @@
-
 package com.example.demo;
 
+import com.example.demo.CategoryEnum.ChoiseCategory;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoneyTrackerApp extends Application {
     private double balance = 0.0;
+    private Label balanceValueLabel;
+    private TextField expenseTextField;
+    private TextField incomeTextField;
+    private PieChart pieChart;
+    private ChoiceBox<ChoiseCategory> expenseCategoryChoiceBox; // Добавляем ChoiceBox для выбора категории расходов
+
+    private ChoiseCategory selectedCategory;
+
+    private Map<ChoiseCategory, Double> categoryBalances = new HashMap<>();
+
 
     public static void main(String[] args) {
         launch(args);
@@ -21,28 +34,49 @@ public class MoneyTrackerApp extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Money Tracker App");
 
-        // Создаем элементы интерфейса
-        Label balanceLabel = new Label("Баланс: ");
-        Label balanceValueLabel = new Label(Double.toString(balance));
-        Label expenseLabel = new Label("Расход: ");
-        TextField expenseTextField = new TextField();
-        Button expenseButton = new Button("Списать");
-        ChoiceBox<String> expenseCategory = new ChoiceBox<>();
-        expenseCategory.getItems().addAll("Транспорт", "Еда и напитки", "Покупки", "Жилье", "Коммунальные услуги", "Автомобиль", "Учеба","Финансовые расходы", "Здоровье", "Связь", "Другое");
-        expenseCategory.setValue("Категории");
+        categoryBalances.put(ChoiseCategory.TRANSPORT, 0.0);
+        categoryBalances.put(ChoiseCategory.FOOD, 0.0);
+        categoryBalances.put(ChoiseCategory.EDUCATION, 0.0);
+        categoryBalances.put(ChoiseCategory.SHOPPING, 0.0);
+        categoryBalances.put(ChoiseCategory.HOUSING, 0.0);
+        categoryBalances.put(ChoiseCategory.LIFE, 0.0);
+        categoryBalances.put(ChoiseCategory.INVESTMENTS, 0.0);
+        categoryBalances.put(ChoiseCategory.OTHERS, 0.0);
 
-        Label incomeLabel = new Label("Доход: ");
-        TextField incomeTextField = new TextField();
+
+        Label balanceLabel = new Label("Баланс: ");
+        balanceValueLabel = new Label(Double.toString(balance));
+
+        pieChart = new PieChart();
+        pieChart.setPrefSize(300, 300);
+
+        expenseTextField = new TextField();
+        incomeTextField = new TextField();
+
+        // Создаем ChoiceBox для выбора категории расходов
+        expenseCategoryChoiceBox = new ChoiceBox<ChoiseCategory>();
+        expenseCategoryChoiceBox.getItems().addAll(ChoiseCategory.values());
+        expenseCategoryChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            selectedCategory = newValue;
+            expenseCategoryChoiceBox.setValue(selectedCategory);
+            System.out.println(selectedCategory);
+        });
+
+
+        Button expenseButton = new Button("Списать");
         Button incomeButton = new Button("Добавить");
 
-        // Обработчик кнопки "Списать"
         expenseButton.setOnAction(e -> {
             try {
                 double expense = Double.parseDouble(expenseTextField.getText());
                 if (expense > 0) {
+                    double currentBalance = categoryBalances.get(selectedCategory);
+                    categoryBalances.put(selectedCategory, currentBalance - expense);
+
                     balance -= expense;
                     balanceValueLabel.setText(Double.toString(balance));
                     expenseTextField.clear();
+                    updatePieChart(selectedCategory, balance);
                 } else {
                     showAlert("Ошибка", "Введите корректную сумму расхода.");
                 }
@@ -51,14 +85,17 @@ public class MoneyTrackerApp extends Application {
             }
         });
 
-        // Обработчик кнопки "Добавить"
         incomeButton.setOnAction(e -> {
             try {
                 double income = Double.parseDouble(incomeTextField.getText());
                 if (income > 0) {
+                    double currentBalance = categoryBalances.get(selectedCategory);
+                    categoryBalances.put(selectedCategory, currentBalance + income);
+
                     balance += income;
                     balanceValueLabel.setText(Double.toString(balance));
                     incomeTextField.clear();
+                    updatePieChart(selectedCategory, balance);
                 } else {
                     showAlert("Ошибка", "Введите корректную сумму.");
                 }
@@ -67,38 +104,12 @@ public class MoneyTrackerApp extends Application {
             }
         });
 
-        // Создаем макет
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(20, 20, 20, 20));
-        gridPane.setVgap(10);
-        gridPane.setHgap(20);
-        gridPane.setStyle("-fx-background-color: #E8E8E8;");
 
-        // Добавляем стили для элементов
-        balanceLabel.setStyle("-fx-font-weight: bold;");
-        balanceValueLabel.setStyle("-fx-font-weight: bold;");
-        balanceValueLabel.setFont(Font.font(16));
-        expenseLabel.setStyle("-fx-font-weight: bold;");
-        expenseButton.setStyle("-fx-background-color: #FF5733; -fx-text-fill: white;");
-        incomeButton.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
-        incomeLabel.setStyle("-fx-font-weight: bold;");
-        expenseCategory.setStyle("-fx-font-size: 14;");
+        VBox root = new VBox(10);
+        root.setPadding(new Insets(20));
+        root.getChildren().addAll(balanceLabel, balanceValueLabel, pieChart, expenseCategoryChoiceBox, expenseTextField, expenseButton, incomeTextField, incomeButton);
 
-        // Создаем разделы для доходов и расходов
-        TitledPane expensePane = new TitledPane("Расходы", createExpensePane(expenseLabel, expenseTextField, expenseCategory, expenseButton));
-        TitledPane incomePane = new TitledPane("Доходы", createIncomePane(incomeLabel, incomeTextField, incomeButton));
-
-        // Создаем аккордеон для разделов
-        Accordion accordion = new Accordion(expensePane, incomePane);
-
-        // Добавляем элементы на макет
-        GridPane.setConstraints(balanceLabel, 0, 0);
-        GridPane.setConstraints(balanceValueLabel, 1, 0);
-        GridPane.setConstraints(accordion, 0, 1, 3, 1);
-
-        gridPane.getChildren().addAll(balanceLabel, balanceValueLabel, accordion);
-
-        Scene scene = new Scene(gridPane, 400, 300);
+        Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -111,17 +122,14 @@ public class MoneyTrackerApp extends Application {
         alert.showAndWait();
     }
 
-    private VBox createExpensePane(Label expenseLabel, TextField expenseTextField, ChoiceBox<String> expenseCategory, Button expenseButton) {
-        VBox expensePane = new VBox(10);
-        expensePane.setPadding(new Insets(10, 10, 10, 10));
-        expensePane.getChildren().addAll(expenseLabel, expenseTextField, expenseCategory, expenseButton);
-        return expensePane;
-    }
+    private void updatePieChart(ChoiseCategory choiseCategory, double balance) {
+        pieChart.getData().clear();
 
-    private VBox createIncomePane(Label incomeLabel, TextField incomeTextField, Button incomeButton) {
-        VBox incomePane = new VBox(10);
-        incomePane.setPadding(new Insets(10, 10, 10, 10));
-        incomePane.getChildren().addAll(incomeLabel, incomeTextField, incomeButton);
-        return incomePane;
+        // Перебираем все категории и добавляем их в диаграмму
+        for (ChoiseCategory category : categoryBalances.keySet()) {
+            double categoryBalance = categoryBalances.get(category);
+            pieChart.getData().add(new PieChart.Data(category.toString(), categoryBalance));
+        }
     }
 }
+
